a5f13635bafbc9936798155ab026a5c1b26c6381
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-03-13 14:54:36 -0700

    completes lab

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+    return 2000 / 100;
+}
+
+const square = (parameter1) => parameter1 * parameter1;
+square();
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add();
